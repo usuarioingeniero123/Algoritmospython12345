12a5bb6f06b5816ff0eb7ebe6377fbb499aad9a8
usuarioingeniero123 <106457167+usuarioingeniero123@users.noreply.github.com>
2022-05-28 20:22:00 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+#ejercicio retador 2
+variablemunicipios= input("escribe el nombre del municipio  ")
+variablenumerodehabitantes= int(input("escribe la cantidad de habitantes"))
+variablemunicipios2= input("escribe el nombre del municipio")
+variablenumerodehabitantes2=int(input("escribe el numero de habitantes de este municipio"))
+variablemunicipios3=input("escribe el numero de habitantes del municiipio  ")
+variablehabitantes3= int(input("Escribe el numero de habitantes"))
+variablesuma= variablenumerodehabitantes+variablenumerodehabitantes2+variablehabitantes3
+variablepromedio= variablesuma/3
+
+print("La cantidad de habitantes total es",variablesuma)
+print(" el promedio de habitantes es ",variablepromedio)
+
+
+
